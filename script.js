6f655a20bc2c3582a04b950e70d17beb0591c1b2
brian-barton <146300586+brian-barton@users.noreply.github.com>
2024-04-18 20:06:56 -0400

    add function to script.js
    I added a new function called playRound with the parameters defined as
    playerSelection and computerSelection. This function was created to play
    one round of "Rock, Paper, Scissors" and return a string that states who
    won the round or if it was a tie. It also console logs the player and
    computers selections for each round.

--- script.js
+++ script.js
@@ -6,3 +6,26 @@ function getComputerChoice() {
 }
 
 const computerSelection = getComputerChoice();
+
+//  plays one round of the game
+function playRound(playerSelection, computerSelection) {
+  console.log("Players selection: " + playerSelection.toUpperCase());
+  console.log("Computers selection: " + computerSelection.toUpperCase());
+  if (playerSelection.toLowerCase() == "rock" && computerSelection == "paper") {
+    return "You Lose! Paper beats Rock!";
+  } else if (playerSelection.toLowerCase() == "paper" && computerSelection == "rock") {
+    return "You Win! Paper beats Rock!";
+  } else if (playerSelection.toLowerCase() == "rock" && computerSelection == "scissors") {
+    return "You Win! Rock beats Scissors!";
+  } else if (playerSelection.toLowerCase() == "scissors" && computerSelection == "rock") {
+    return "You Lose! Rock beats Scissors!";
+  } else if (playerSelection.toLowerCase() == "paper" && computerSelection == "scissors") {
+    return "You Lose! Scissors beats Paper!";
+  } else if (playerSelection.toLowerCase() == "scissors" && computerSelection == "paper") {
+    return "You Win! Scissors beats Paper!";
+  } else if (playerSelection.toLowerCase() == computerSelection) {
+    return "No one wins! It's a tie!";
+  } else {
+    return "Player did not make a choice";
+  }
+}
